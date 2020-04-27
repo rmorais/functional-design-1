907a47391e262017ce9c25b7a3e221804dc157a2
Rui Morais <rui.morais@pm.me>
2020-04-27 14:46:01 +0100

    Add notes

--- src/main/scala/net/degoes/workspace.scala
+++ src/main/scala/net/degoes/workspace.scala
@@ -15,4 +15,21 @@ object workspace {
   //    START :
   //    LUNCH :
   //    END   :
+
+  //Product Type
+  final case class Person(name: String, age: Int)
+
+  //Sum Type Contains exactly one of
+  sealed trait ShippingMethod
+  object ShippingMethod {
+    object Fedex       extends ShippingMethod
+    case object DHL    extends ShippingMethod
+    case object Postal extends ShippingMethod
+  }
+
+  def t(sm: ShippingMethod): Unit =
+    sm match {
+      case ShippingMethod.Fedex => println("fedex")
+      case _                    => println("other")
+    }
 }
